cb0deebb9d65b375dcf4be8b20adaa122d870570
Arno VAN WYK <arnovan-@e4r20p3.jnb.42.fr>
2017-05-27 13:29:10 +0200

    controls

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: arnovan- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/27 08:38:42 by arnovan-          #+#    #+#             */
-/*   Updated: 2017/05/27 13:04:15 by arnovan-         ###   ########.fr       */
+/*   Updated: 2017/05/27 13:28:56 by arnovan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,52 +19,76 @@
 #include "Ship.hpp"
 #include "Fighter.hpp"
 #include "Destroyer.hpp"
-
+/*
 void shipInit() {
 
-	std:: string	choice;
+	std::string		choice;
 	std::string		name;
-	
+	int ch = ;
 
-	std::cout << "Hey Bro, Welcome to our totally rad Game\n" << std::endl;
-	std::cout << "Choose your Ship Type: (1)Fighter or (2)Destroyer" <<std::endl;
-	std::cin >> choice;
+	clear();
+	mvprintw(0, 0, "Choose your Ship Type: (1)Fighter or (2)Destroyer");
 
-	if (choice == "Fighter" || choice == "1") {
+	refresh();
+	if (ch != 1)
+	{
+		ch = getch();
+		switch (ch)
+		{
+			case '1':
+				clear();
+				mvprintw(0, 0, "selected 1");
+				break;
+			case '2':
+				clear();
+				mvprintw(0, 0, "selected 2");
+				break;
+		}
+	}
+	*/
+	/*	
+
+		std::cout << "Hey Bro, Welcome to our totally rad Game\n" << std::endl;
+		std::cout << "Choose your Ship Type: (1)Fighter or (2)Destroyer" <<std::endl;
+		std::cin >> choice;
+
+		if (choice == "Fighter" || choice == "1") {
 		std::cout << "Enter a name for your radical fighter: " << std::endl;
 		std::cin >> name;
-		if (name == " ")
-			name = "Deathbringer";
 		Fighter ship(name);
-	}
-	else if (choice == "Destroyer" || choice == "2") {
+		}
+		else if (choice == "Destroyer" || choice == "2") {
 		std::cout << "Enter a name for your awesome destroyer: " << std::endl;
 		std::cin >> name;
 		Destroyer ship(name);
-	}
-}
+		}
+		*/
+//}
 
 int main(void)
 {
-	shipInit();
-	
 	initscr();
 	noecho();
 	cbreak();
 	keypad(stdscr, TRUE);
 	nodelay(stdscr, TRUE);
 	curs_set(FALSE);
-//From tutorial////////
+
+
+//	shipInit();
+	//From tutorial////////
 	int ch = 0; 
-	int x = 10, y = 10;
+	int x = 0, y = 0;
 	int max_x = 0, max_y = 0;
 	int next_x = 0;
 	int direction = 1;
-//////////////////////
+	//////////////////////
 
-	
 
-	std::cin.get();
+	getmaxyx(stdscr, max_y, max_x);
+	x = max_x / 2;
+	y = max_y - 1;
+//	std::cin.get();
 	while (1) // Main Gameloop
 	{
 		ch = getch();
