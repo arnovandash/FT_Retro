91d867c4537b4b56bc4b44e4b1588f7d6c652e7f
Arno VAN WYK <arnovan-@e4r20p3.jnb.42.fr>
2017-05-27 12:31:16 +0200

    basic ncurses frame

--- main.cpp
+++ main.cpp
@@ -6,20 +6,56 @@
 /*   By: arnovan- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/27 08:38:42 by arnovan-          #+#    #+#             */
-/*   Updated: 2017/05/27 08:49:06 by arnovan-         ###   ########.fr       */
+/*   Updated: 2017/05/27 12:29:34 by arnovan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <ncurses.h>
+#include <unistd.h>
 
 int main()
 {
 	initscr();
 	noecho();
+	cbreak();
+	keypad(stdscr, TRUE);
+	nodelay(stdscr, TRUE);
 	curs_set(FALSE);
-
 	std::cin.get();
+
+	int ch = 0; 
+	int x = 10, y = 10;
+	int max_x = 0, max_y = 0;
+	int next_x = 0;
+	int direction = 1;
+
+	while (1) // Main Gameloop
+	{
+		ch = getch();
+		switch (ch)
+		{
+			case KEY_UP:
+				std::cout << ch << std::endl;
+				endwin();
+				return (0);
+				break;
+			case KEY_DOWN:
+				std::cout << "Happy " << std::endl;
+		}
+
+		//Start of render code
+		clear();
+		mvprintw(y, x, "o");
+		x++;
+
+		refresh();
+		usleep(20000);
+		//end of renderer code
+
+	}
+
+
 	endwin();
 	return (0);
 }
